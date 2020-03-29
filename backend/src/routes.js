f4fbe8652cf3fdb496b0ff95409a108d7d0f6750
Julio Lima <julio_flima@hotmail.com>
2020-03-29 18:58:42 -0300

    error in deleting

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
+const OngController = require('./controllers/ongController')
+const IncidentController = require('./controllers/IncidentController')
+
 const routes = express.Router();
 
-routes.post("/ongs", (request, response) => {
-    const data = request.body
+routes.get("/ongs", OngController.index)
+routes.post("/ongs", OngController.create)
 
-    console.log(data);
-
-    return response.json();
-})
+routes.get("/incidents", IncidentController.index)
+routes.post("/incidents", IncidentController.create)
+routes.delete("/incidents/:id", IncidentController.delete)
 
 
 module.exports = routes
